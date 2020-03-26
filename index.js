3122ede82423a932e4c7c2d4795cdcfcbdcfe770
amin havasi <pau.ahq@gmail.com>
2020-03-27 00:20:35 +0430

    first

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 const express = require('express');
 const socketIo = require('socket.io')
-const http=require('http');
-const app=express();
-require('dotenv').config()
-const server=http.createServer(app);
-const io=socketIo(server);
-
-const router=require('./router')
-app.use(router)
-server.listen(process.env.PORT||5000,()=>{
-	console.log(`server is running on port ${process.env.PORT||5000}`)
-})
+const http = require('http');
