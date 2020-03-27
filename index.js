d4fb9d0521467c3e9226e4d04c54de612ca25bad
amin havasi <pau.ahq@gmail.com>
2020-03-28 00:18:40 +0430

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const socketIo = require('socket.io')
 const http = require('http');
+const router=require('./router.js')
+const app=express();
+require('dotenv').config();
+const server=http.createServer(app);
+app.use(router)
+const io=socketIo(server)
+io.on('connection',(socket)=>{
+  console.log('we have a new connection')
+  socket.on('disconnect',()=>{
+    console.log('User had left')
+  })
+})
+server.listen(process.env.PORT,()=>{
+  console.log(`server is running on port ${process.env.PORT}`)
+
+})
