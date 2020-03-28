5009954a82279d9fa15c67caf723a6bb5b3fae82
amin havasi <pau.ahq@gmail.com>
2020-03-29 00:49:09 +0430

    user created

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ const router=require('./router.js')
 const app=express();
 require('dotenv').config();
 const server=http.createServer(app);
+const {addUser,getUser,getUsersInRoom,removeUser}=require('./user')
 app.use(router)
 const io=socketIo(server)
 io.on('connection',(socket)=>{
-  console.log('we have a new connection')
+  socket.on('join',({name,room},callback)=>{
+  const {error,user}=addUser({id:socket.id,name,room});
+  if(error) return callback(error);
+  socket.emit('message',{user:'admin',text:`${user.name},welcome to the room ${user.room}`})
+  socket.broadcast.to(user.room).emit('message',{user:'admin',text:`${user.name} has join`})
+  socket.join(user.room);
+  callback();
+
+
+
+  })
   socket.on('disconnect',()=>{
     console.log('User had left')
   })
