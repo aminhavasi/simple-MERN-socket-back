d43c42125e1e7e30f206aae3294a33a9eeea36b6
amin havasi <pau.ahq@gmail.com>
2020-03-30 03:17:27 +0430

    messagess

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ io.on('connection',(socket)=>{
 
 
   })
+  socket.on('sendMessage',(message,callback)=>{
+    const user=getUser(socket.id);
+    io.to(user.room).emit('message',{user:user.name,text:message});
+
+    callback();
+
+  })
   socket.on('disconnect',()=>{
     console.log('User had left')
   })
