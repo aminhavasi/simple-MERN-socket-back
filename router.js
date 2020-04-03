6bbc16af67f6f892e05c83a53b2d6c2ee09be02b
amin havasi <pau.ahq@gmail.com>
2020-04-03 04:35:10 +0430

    second

--- router.js
+++ router.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.get('/', (req, res) => {
-	res.send('server is up and running ')
-})
+    res.send('server is up and running ');
+});
 
 module.exports = router;
